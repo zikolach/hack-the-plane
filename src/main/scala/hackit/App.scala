8234072c45a66135486ded8d270cadb4d0d0415b
Nikolay Kushin <zikolach@gmail.com>
2014-12-05 19:27:38 +0100

    DOM example

--- src/main/scala/hackit/App.scala
+++ src/main/scala/hackit/App.scala
@@ -1,9 +1,31 @@
 package hackit
 
+import java.util.Date
+
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExport
 
 object App extends JSApp {
+
+  var counter = 0
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExport
+  def addMessage(): Unit = {
+    appendPar(document.body, counter.toString)
+    counter = counter + 1
+  }
+
   def main(): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello Zikolach")
   }
 }
